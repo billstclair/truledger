8b76916449d0d141883c440215666789b897c3d3
Bill St. Clair <billstclair@gmail.com>
2009-03-29 21:06:44 -0400

    find/replace typo in copyright

--- src/utilities.lisp
+++ src/utilities.lisp
@@ -102,12 +102,7 @@
   (base64-string-to-string string))
 
 (defun strcat (&rest strings)
-  (if (null strings)
-      ""
-      (let ((res (car strings)))
-        (dolist (s (cdr strings))
-          (setq res (concatenate 'string res s)))
-        res)))
+  (apply #'concatenate 'string strings))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -117,7 +112,7 @@
 ;;; you may not use this file except in compliance with the License.
 ;;; You may obtain a copy of the License at
 ;;;
-;;;     http:;;;www.apache.org/licenses/LICENSE-2.0
+;;;     http://www.apache.org/licenses/LICENSE-2.0
 ;;;
 ;;; Unless required by applicable law or agreed to in writing, software
 ;;; distributed under the License is distributed on an "AS IS" BASIS,
