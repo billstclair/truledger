46daab6a9d9ae82b6a4e857edc07e5db627790ef
Bill St. Clair <billstclair@gmail.com>
2009-06-05 22:55:08 -0400

    Fix the web server shut down code

--- src/server-web.lisp
+++ src/server-web.lisp
@@ -728,8 +728,11 @@ openssl x509 -in cert.pem -text -noout
                  (process-wait "Server stopper" (lambda () started)))
                ;; Need to make a request in order to get the server to shut down
                (ignore-errors
-                 (dotimes (i 3)
-                   (drakma:http-request (format nil "http://localhost:~d/" port)))))))))
+                 (let ((https (type (port-acceptor port) 'hunchentoot:ssl-acceptor)))
+                 (drakma:http-request
+                  (format nil "~a://localhost:~d/"
+                          (if https "https" "http") httport)
+                  :redirect nil))))))))
 
 (defun send-bank-request (uri &optional msg-p)
   (drakma:http-request
