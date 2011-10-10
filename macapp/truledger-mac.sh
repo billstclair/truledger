15c10c275fe0f908181a7854c89d5994a4e56a9f
Bill St. Clair <billstclair@gmail.com>
2011-10-10 17:33:40 -0400

    macapp/ dir with script and resources to build Truledger.dmg

---
+++ macapp/truledger-mac.sh
@@ -0,0 +1,8 @@
+if [[ -f truledger-dx86cl64 && `uname -m` == 'x86_64' ]]; then
+  ./truledger-dx86cl64;
+elif [[ -f truledger-dx86cl32 ]]; then
+  ./truledger-dx86cl32;
+else
+  echo "Can't find executable.";
+  while true; do sleep 1; done;
+fi
